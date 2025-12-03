d87bd407c3eb849585dc9c1af453a8174e92b425
Joel Ababio Dartey <joeldartey71@gmail.com>
2025-12-03 08:30:53 +0100

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -18,10 +18,6 @@ const ProtectedRoute: React.FC<{
     return <Navigate to={"/"} replace />;
   }
 
-  if (data?.role !== "admin" && data.role !== "user") {
-    return <Navigate to={"/"} replace />;
-  }
-
   if (isLoading) {
     return (
       <div className="fixed w-full z-50 backdrop-blur bg-black/70 flex justify-center items-center h-full left-0 bottom-0">
@@ -35,6 +31,11 @@ const ProtectedRoute: React.FC<{
 
   if (isError) {
     localStorage.removeItem("sch_token");
+    return <Navigate to="/" replace />;
+  }
+
+  if (data?.role !== "admin" && data.role !== "user") {
+    return <Navigate to={"/"} replace />;
   }
 
   return <>{children}</>;
